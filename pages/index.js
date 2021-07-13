1bac02fd59283a891fef04f2daffecf700bda65b
Oliver Ripps <oliver.ripps@com>
2021-07-13 16:13:22 -0400

    push dummy

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,16 @@ import { Heading, Page } from "@shopify/polaris";
 
 const Index = () => (
   <Page
-  title  = 'Product Selector'
-    primaryAction = {{
-      content: 'Select products',
-      onAction: () => console.log('clicked')
-    }
-    }
+    title="Product Selector"
+    primaryAction={{
+      content: "Select products",
+      onAction: () => console.log("clicked"),
+    }}
+    title="Product Selector 2"
+    primaryAction={{
+      content: "Select products",
+      onAction: () => console.log("clicked"),
+    }}
   />
 );
 
