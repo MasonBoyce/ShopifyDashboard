53d2a357ddbc8e1e2295797b0c40008b951002dd
Mason Boyce <boyce.mason@gmail.com>
2021-07-13 22:44:40 -0400

    finished basic product selecting and reporting to console

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,29 @@ import { ResourcePicker} from "@shopify/app-bridge-react";
 
 class Index extends React.Component{
   state  = {open: false }
-  render()
+  render(){
   return(
   <Page
     title="Product Selector"
     primaryAction={{
       content: "Select products",
-      onAction: () => console.log("clicked"),
-    }})
+      onAction: () => this.setState({open:true})
+    }}
   >
   <ResourcePicker
     resourceType ='Product'
     open = {this.state.open}
+    onCancel ={() => this.setState({open: false})}
+    onSelection = {(resources) => this.handleSelection(resources)}
   />
   </Page>
 )}
+
+handleSelection = (resources)  => {
+const idFromResources  = resources.selection.map((product) => product.id)
+this.setState({open: false})
+console.log(idFromResources)
+}
+}
 
 export default Index;
