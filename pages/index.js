33368011b18d09a2a0f055b17f63f904d1afe0ca
Mason Boyce <boyce.mason@gmail.com>
2021-07-15 18:21:35 -0400

    Added more Selection Features

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,47 @@
-import { Heading, Page} from "@shopify/polaris";
+
+import React from 'react';
+import { Heading, Page, Layout, EmptyState} from "@shopify/polaris";
 import { ResourcePicker} from "@shopify/app-bridge-react";
+import store from 'store-js';
+import ResourceListWithProducts from './components/ResourceList';
+
 
 class Index extends React.Component{
   state  = {open: false }
   render(){
-  return(
-  <Page
-    title="Product Selector"
-    primaryAction={{
-      content: "Select products",
-      onAction: () => this.setState({open:true})
-    }}
-  >
-  <ResourcePicker
-    resourceType ='Product'
-    open = {this.state.open}
-    onCancel ={() => this.setState({open: false})}
-    onSelection = {(resources) => this.handleSelection(resources)}
-  />
-  </Page>
-)}
+    return(
+      <Page
+      title="Product Selector"
+      primaryAction={{
+        content: "Select products",
+        onAction: () => this.setState({open:true})
+      }}
+      >
+      <ResourcePicker
+      resourceType ='Product'
+      open = {this.state.open}
+      onCancel ={() => this.setState({open: false})}
+      onSelection = {(resources) => this.handleSelection(resources)}
+      />
+      <EmptyState
+      heading="Discount your products temporarily"
+      action={{
+        content: 'Select products',
+        onAction: () => this.setState({ open: true }),
+      }}
+      
+      >
+      <p>Select products to change their price temporarily.</p>
+      </EmptyState>
+      <ResourceListWithProducts />
+      </Page>
+    )}
 
-handleSelection = (resources)  => {
-const idFromResources  = resources.selection.map((product) => product.id)
-this.setState({open: false})
-console.log(idFromResources)
-}
-}
+    handleSelection = (resources)  => {
+      const idFromResources  = resources.selection.map((product) => product.id)
+      this.setState({open: false})
+      store.set('ids',idFromResources);
+    };
+  }
 
-export default Index;
+  export default Index;
